23b246208ebb5a671e19b27c075d67d12e192ef1
Echoloids <jericho16anonuevo@gmail.com>
2025-03-06 22:21:24 +0800

    Website Updated

--- resources/views/website/general.blade.php
+++ resources/views/website/general.blade.php
@@ -1,8 +1,7 @@
 
    <div class="general_section layout_padding" id="aboutus">
       <div class="container">
-         <h1 class="general_taital">New Ground Generation Church</h1> <br>
-         <hr style="height:1px;width:15%;color:gray;background-color: lightgray;margin-bottom:0px"> <br>
+         <h1 class="general_taital">New Ground Generation Church</h1>
          <p class="general_text">The New Ground Generation Church was founded in 1985 by Pastor Evelyn Carter in Springfield, Missouri, as a small community dedicated to embracing modern worship practices while honoring traditional Christian values. The church grew rapidly in the 1990s, fueled by its unique focus on youth empowerment and community outreach, including the launch of its renowned "Fresh Start" program for underprivileged families. Today, it stands as a beacon of faith and renewal, with branches across five states and a vibrant global online congregation.</p>
          <br>
 
